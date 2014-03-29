0604d9df83b6650df717128d71310190ac92b15c
Jacob Miner <zyko101@gmail.com>
2014-03-29 03:21:49 -0700

    Added file parsing
    File parsing for default template file is done (probably)

---
+++ RPGame.py
@@ -0,0 +1,7 @@
+import globalTools
+#simple test game
+#should create an admin, and two users
+#each user should take a turn, attack a creature, and kill it
+#creature should inflict poison on one of the players
+#one player should level up, other should die
+globalTools.createCharacterSheet("Joe")
--- globalTools.py
+++ globalTools.py
@@ -8,12 +8,32 @@ Tools for all users
 """
 
 #create a simple character sheet using user input
-def createCharacterSheet():
+def createCharacterSheet(character):
+	default = readDefaultSheet()
+	for field in default:
+		print "enter the " + field 
 	return
+
+def readDefaultSheet():
+	default = readCharacterSheet("template")
+	return default
 
 #read in all the character values into the game
 def readCharacterSheet(character):
-	return
+    fields = {}
+    fieldName = ''
+    fieldValue = ''
+
+    with open(character + ".char") as f:
+        for line in f:
+            if not line.startswith("#"):
+                line = stripWhitspace(line)
+                temp = line.split("=")
+                fieldName = temp[0]
+                fieldValue = temp[1]
+                fields[fieldName] = fieldValue
+
+    return fields 
 
 #modify a field within a character sheet, setting it to value
 def modifyValue(character, field, value):
@@ -74,13 +94,13 @@ def getXP(character):
 
 #checks the xp needed to get to the next level 
 def getXPtoNextLevel(level):
-	return
+	return 0
 
 #check to see if a character has enough xp to level up
 def checkLevelUp(character):
 	currXP	  = getXP(character)
 	currLevel = getLevel(character)
-	while (currXP > getXPtoNextLevel(level))
+	while (currXP > getXPtoNextLevel(level)):
 		currLevel = levelUp(character)
 
 #level up the character
@@ -89,3 +109,8 @@ def levelUp(character):
 
 def checkGameMode():
 	return
+
+def stripWhitspace(text):
+	temp = text.rstrip()
+	temp = temp.lstrip()
+	return temp
