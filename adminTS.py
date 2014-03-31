dc4d39d0bbd0af0277c28d75b02d9861973a425c
Jacob Miner <zyko101@gmail.com>
2014-03-31 00:39:17 -0700

    Adding game logic to admin
    Basic game logic in the works

--- adminTS.py
+++ adminTS.py
@@ -1,12 +1,15 @@
 # admin side of game
 # wait for clients to connect and ready up
 # download characters
+# add characters to current game
 # tell clients to start game
 # while playing
 # 	fastest player gets turn
 # 	wait for player's move
 # 		react to players move (summon monsters as new players)
 #		move to next player
+gameInProgress = False
+gameState = "LOBBY"
 
 # wait for players to connect to the admin
 def waitForPlayers():
@@ -24,11 +27,30 @@ def getCharacters():
 def alertPlayers(messsage):
 	return
 
-# begin game loop, choose turn, wait for player, react to player
+# begin game loop, choose turn.
+# if turn is players
+#	 wait for player, react to player
+# else 
+#	make npc move
 def beginGame():
+	while gameInProgress:
+		playerMove = chooseTurn()
+		if isPlayer(playerMove):
+			alertPlayers(playerMove)
+			waitForMove(playerMove)
+			adminMove = getReaction()
+			alertPlayers(adminMove)
+		else:
+			creatureMove = getNPCMove(playerMove)
+			alertPlayers(creatureMove)
+			handleInturrupt()
 	return
 
-# pick the player with the highest speed and the lowest number of turns
+# checks to see if the character is player controlled or admin controlled
+def isPlayer(character):
+	return
+
+# pick the player with the highest speed and the lowest number of turns, return characters name. if the 
 def chooseTurn():
 	return
 
@@ -36,10 +58,23 @@ def chooseTurn():
 def waitForMove():
 	return
 
-# react to user input
+# react to user input, using input from the admin
 def getReaction():
+	return
+
+#add a player to the game  (human = true or false)
+def addPlayer(character, human):
 	return
 
 # handle an inturrupting move
 def handleInturrupt():
 	return
+
+def main():
+	waitForPlayers()
+	while not playersReady():
+		continue
+
+	getCharacters()
+	alertPlayers("GAME START")
+	beginGame()
