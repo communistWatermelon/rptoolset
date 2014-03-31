0437cc742a4de282eb33f08dfebd36a5cd26f1b2
Jacob Miner <zyko101@gmail.com>
2014-03-31 00:22:50 -0700

    Added pseudocode for admin and client code
    Added placeholder functions to handle very basic gameplay.

---
+++ RPclient.py
@@ -0,0 +1,41 @@
+# client console side of game
+# connect to admin
+# choose or create character
+# ready up
+# send characters
+# wait for turn (display stats)
+# 	send move to admin
+import globalTools
+
+# connects the client to the admin, using the ipaddress and username provided. 
+def connectToAdmin(ipaddress, username):
+	return
+
+# displays all characters in the character folder, and allows user to select one to play with.
+# first display stats, then confirm character
+def chooseCharacter():
+	return
+
+# begins gameplay. Waits for turn, then gets the users move
+def beginGame():
+	return
+
+# obtains input from the user to send to admin
+def getMove():
+	return
+
+# check if it's your turn, then sends move to the admin
+def sendMove():
+	return
+
+# sends a move that isn't turn specific. Not sure if it's necessary yet?
+def sendInturrupt():
+	return
+
+# check player's current stats
+def checkStats():
+	return
+
+# check player's equipment
+def checkEquipment():
+	return
---
+++ adminTS.py
@@ -0,0 +1,45 @@
+# admin side of game
+# wait for clients to connect and ready up
+# download characters
+# tell clients to start game
+# while playing
+# 	fastest player gets turn
+# 	wait for player's move
+# 		react to players move (summon monsters as new players)
+#		move to next player
+
+# wait for players to connect to the admin
+def waitForPlayers():
+	return
+
+# check if players are ready to play
+def playersReady():
+	return
+
+# download character sheets from each client
+def getCharacters():
+	return
+
+# alert all players with message
+def alertPlayers(messsage):
+	return
+
+# begin game loop, choose turn, wait for player, react to player
+def beginGame():
+	return
+
+# pick the player with the highest speed and the lowest number of turns
+def chooseTurn():
+	return
+
+# wait for user input 
+def waitForMove():
+	return
+
+# react to user input
+def getReaction():
+	return
+
+# handle an inturrupting move
+def handleInturrupt():
+	return
